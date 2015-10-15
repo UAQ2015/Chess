85388718ec76136fa55755a508cbac83b79b47ab
Pepe <pepe@debian>
2015-10-15 13:53:48 -0500

    Added file bishop.cpp

---
+++ bishop.cpp
@@ -0,0 +1,19 @@
+#include "bishop.h"
+#include <iostream>
+
+Bishop::Bishop(int _player, int _x, int _y)
+        :Piece(_player, _x, _y)
+{
+        std::cout<<"Creating a Bishop\n";
+}
+
+Bishop::~Bishop()
+{
+        std::cout<<"Killing a Bishop\n";
+}
+
+void Bishop::Draw()
+{
+        std::cout<<"Bishop\n";
+}
+
