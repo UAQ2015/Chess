26c3832d4dba3b89b2700dac65af5127737f7d67
Pepe <pepe@debian>
2015-10-15 13:54:27 -0500

    Added file king.cpp

---
+++ king.cpp
@@ -0,0 +1,20 @@
+#include "king.h"
+#include <iostream>
+
+King::King(int _player, int _x, int _y)
+        :Piece(_player, _x, _y)
+{
+        std::cout<<"Creating a King\n";
+}
+
+King::~King()
+{
+        std::cout<<"Killing a King?\n";
+}
+
+void King::Draw()
+{
+        std::cout<<"King\n";
+}
+
+
