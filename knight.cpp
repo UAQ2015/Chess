306d6c0d56c3a69133f3748df8088b3762ce728a
Pepe <pepe@debian>
2015-10-15 13:55:12 -0500

    Added file knight.cpp

---
+++ knight.cpp
@@ -0,0 +1,20 @@
+#include "knight.h"
+#include <iostream>
+
+Knight::Knight(int _player, int _x, int _y)
+        :Piece(_player, _x, _y)
+{
+        std::cout<<"Creating a knight\n";
+}
+
+Knight::~Knight()
+{
+        std::cout<<"Killing a knight\n";
+}
+
+void Knight::Draw()
+{
+        std::cout<<"Knight\n";
+}
+
+
