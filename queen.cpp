7148cd463e495cca2c382899f4b8a223aa326cba
Pepe <pepe@debian>
2015-10-15 13:56:06 -0500

    Added file queen.cpp

---
+++ queen.cpp
@@ -0,0 +1,20 @@
+#include "queen.h"
+#include <iostream>
+
+Queen::Queen(int _player, int _x, int _y)
+        :Piece(_player, _x, _y)
+{
+        std::cout<<"Creating a Queen\n";
+}
+
+Queen::~Queen()
+{
+        std::cout<<"Killing a Queen :s\n";
+}
+
+void Queen::Draw()
+{
+        std::cout<<"Queen\n";
+}
+
+
