4793afdfefe390f403acdbd5922dda6c9268bfe8
Pepe <pepe@debian>
2015-10-15 13:56:38 -0500

    Added file rook.cpp

---
+++ rook.cpp
@@ -0,0 +1,18 @@
+#include "rook.h"
+#include <iostream>
+
+Rook::Rook(int _player, int _x, int _y)
+	:Piece(_player, _x, _y)
+{	
+	std::cout<<"Building a rook\n";
+}
+
+Rook::~Rook()
+{
+	std::cout<<"Destroying a rook *BOOM*\n";
+}
+
+void Rook::Draw()
+{
+	std::cout<<"Rook\n";
+}
